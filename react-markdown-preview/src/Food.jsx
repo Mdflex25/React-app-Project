e4be37fa58262ff7d75e5c310448510c40ed6f9d
Mdflex25 <okekefelixebuka@gmail.com>
2025-10-26 22:45:00 +0100

    Initial commit

---
+++ react-markdown-preview/src/Food.jsx
@@ -0,0 +1,16 @@
+
+function Food(){
+
+    const food1 = "Fried rice";
+    const food2 = "Spaghetti";
+
+    return(
+        <ul>
+            <li>Jollof rice</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    );
+}
+
+export default Food
